611ff0c65131e354a283cb55f512baf80e21a526
sugarapple1290 <zeeshanhadiya4@gmail.com>
2025-09-20 01:32:49 -0700

    1

---
+++ activity1.py
@@ -0,0 +1,13 @@
+#  To ask for users favorite  ice cream flavour
+Icecream=input("what is your favorite icecream flavour:  ")
+print(Icecream)
+
+# to ask for how much icecream
+b=int(input("how many icecream do you want:"))
+
+print(b)
+print(type(b))
+# display the total cost of purchase
+c=5*b
+print(c)
+
